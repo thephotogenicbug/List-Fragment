e54f163c4230eb4449c7bae838d7b688f8475350
thephotogenicbug <navinkumarachari@gmail.com>
2022-11-02 13:12:25 +0530

    Add ListFragment

--- app/src/main/java/com/naveen/listfragment/MainActivity.kt
+++ app/src/main/java/com/naveen/listfragment/MainActivity.kt
@@ -2,10 +2,22 @@ package com.naveen.listfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager : FragmentManager = supportFragmentManager
+        val fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
+
+        val myListFragment = MyListFragment()
+
+        fragmentTransaction.add(R.id.frame, myListFragment)
+
+        fragmentTransaction.commit()
+
     }
 }
---
+++ app/src/main/java/com/naveen/listfragment/MyListFragment.kt
@@ -0,0 +1,30 @@
+package com.naveen.listfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.ListFragment
+
+class MyListFragment : ListFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val arrayAdapter = activity?.let {
+            ArrayAdapter.createFromResource(it, R.array.cities, android.R.layout.simple_list_item_1) }
+
+        listAdapter = arrayAdapter
+    }
+}
